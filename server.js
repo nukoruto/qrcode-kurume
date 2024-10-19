f424078a539e0d2e0d1bc29f63b90e9287c83921
孔明 小形 <df360a@gmail.com>
2024-10-19 21:33:54 +0900

    IPアドレスを動的に取得できるようにし、リンクをローカルIPアドレスに変更しました

--- server.js
+++ server.js
@@ -1,6 +1,25 @@
+//importみたいなしりーず
 const express = require('express');
+const os = require('os');
 const app = express();
 const port = 3002;
+
+// Wi-FiアダプタのIPv4アドレスを取得する関数
+function getWifiIPAddress() {
+  const interfaces = os.networkInterfaces();
+  for (const name of Object.keys(interfaces)) {
+      if (name.includes('Wi-Fi') || name.includes('Wireless')) {
+          for (const iface of interfaces[name]) {
+              if (iface.family === 'IPv4' && !iface.internal) {
+                  return iface.address;
+              }
+          }
+      }
+  }
+  return '127.0.0.1';
+}
+
+const localIP = getWifiIPAddress();
 
 // publicディレクトリを静的ファイルのルートディレクトリとして指定
 app.use(express.static('public'));
@@ -11,6 +30,6 @@ app.get('/', (req, res) => {
 });
 
 // サーバー起動
-app.listen(port, '127.0.0.1', () => {  // ホストを '127.0.0.1' に指定
-  console.log(`Server running at http://127.0.0.1:${port}/`);
+app.listen(port, localIP, () => {
+  console.log(`Server running at http://${localIP}:${port}/`);
 });
